37e4e8abe97dab6d5fbf57e8ac49f88705d6c5cb
adityamandal-Developer <mandaladitya901@gmail.com>
2025-02-27 20:51:08 +0530

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import { Ripple } from "@/components/ripple";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  // const plans = [];
+
   return (
-    <main>
-      <h1>Instagram DM SASS</h1>
+    <main className="overflow-hidden">
+      <section className="h-screen w-screen flex flex-col justify-center items-center">
+        <div className="text-center w-4/6">
+          <h1 className="text-2xl sm:text-6xl text-primary font-bold italic">
+            DMPilot {"–"} Automate Your Instagram DMs Effortlessly!🚀
+          </h1>
+        </div>
+        <div className="flex gap-2 p-2">
+          <Button className="bg-background border text-primary hover:text-foreground">
+            Get started
+          </Button>
+          <Button className="" variant={"outline"}>
+            Learn more
+          </Button>
+        </div>
+        <Ripple mainCircleSize={200} numCircles={20} />
+      </section>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
   	extend: {
   		colors: {
@@ -65,6 +66,19 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		animation: {
+  			ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite'
+  		},
+  		keyframes: {
+  			ripple: {
+  				'0%, 100%': {
+  					transform: 'translate(-50%, -50%) scale(1)'
+  				},
+  				'50%': {
+  					transform: 'translate(-50%, -50%) scale(0.9)'
+  				}
+  			}
   		}
   	}
   },
